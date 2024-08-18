6d46aee7829729965e21d8055bec8bf68f0745cf
rkmiller131 <rkmiller131@gmail.com>
2024-08-18 11:46:56 -0700

    Added jest for timestamp check for Insomniac

---
+++ src/utils/__tests__/isTimestampInWindow.test.js
@@ -0,0 +1,26 @@
+const isTimestampInWindow = require('../isTimestampInWindow');
+
+describe(('Timestamp in Window Check'), () => {
+  it('should correctly return true if timstamp is in window', () => {
+    const result = isTimestampInWindow(1724005057726, 10, 13);
+    expect(result).toBeTruthy();
+  });
+
+  it('returns true for timestamp exactly at the end time', () => {
+    const timestamp = new Date('2024-08-18T13:00:00').getTime();
+    const result = isTimestampInWindow(timestamp, 10, 13);
+    expect(result).toBeTruthy();
+  });
+
+  it('should correctly return false if timstamp is outside window', () => {
+    const result = isTimestampInWindow(1724005057726, 2, 4);
+    expect(result).not.toBeTruthy();
+  });
+
+  it('should return false for incorrectly formatted start and end time arguments', () => {
+    const incorrectCall1 = isTimestampInWindow(1724005057726, '10AM', '1PM');
+    const incorrectCall2 = isTimestampInWindow(1724005057726, '10:00', '1:00');
+    expect(incorrectCall1).not.toBeTruthy();
+    expect(incorrectCall2).not.toBeTruthy();
+  });
+})
---
+++ src/utils/isTimestampInWindow.js
@@ -0,0 +1,22 @@
+module.exports = function isTimestampInWindow(timestamp, startTime, endTime) {
+  // converting into Date object to manipulate and compare times
+  const date = new Date(timestamp);
+
+  // get the difference in minutes between the UTC and the local time (ahead or behind utc standard)
+  const localOffset = date.getTimezoneOffset();
+  // use the local offset to adjust the date to reflect the local timezone offset.
+  // This adjusts the date to represent the exact moment in the user's local timezone.
+  // negative localOffsets mean ahead and positives mean behind UTC (kinda counterintuitive), so subtract
+  date.setMinutes(date.getMinutes() - localOffset);
+  console.log('date is ', date)
+
+  // redefine the start and end times. Since we only care about the time in milliseconds (getTime()),
+  // rest of the date can be generic epoch of today's date, with that localOffset adjustment
+  const start = new Date();
+  start.setHours(startTime, 0 - localOffset, 0);
+  const end = new Date();
+  end.setHours(endTime, 0 - localOffset, 0);
+
+  // check if the timestamp falls within the time frame (returning true or false)
+  return date.getTime() >= start.getTime() && date.getTime() <= end.getTime();
+}
