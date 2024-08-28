a04f8b57984be5523cdc6cfbfab7c2007c166f38
rkmiller131 <rkmiller131@gmail.com>
2024-08-28 06:43:41 -0700

    Added back art channel check to achievement

--- src/events/achievementChecks/artAficionado.js
+++ src/events/achievementChecks/artAficionado.js
@@ -1,10 +1,13 @@
 const findAndGiveAchievement = require('../../utils/findAndGiveAchievement');
+const isMessageArtRelated = require('../../utils/isMessageArtRelated');
 const { getUserDocument, updateUserChannels } = require('../../utils/server.collection');
 
 module.exports = async function checkArtAficionado(message, guildId, userId) {
   const { user } = await getUserDocument(guildId, userId);
-  const channelName = message.channel.name;
+  const artRelatedChannel = isMessageArtRelated(message);
+  if (artRelatedChannel !== true) return;
 
+  const channelName = message.channel.name;
   if (user.channelsParticipatedIn.get(channelName) !== 10) return;
 
   await findAndGiveAchievement('Art Aficionado', user, message, guildId, userId);
