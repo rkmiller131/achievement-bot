90f53505f30caa60170388de9b33b8697cdb874a
rkmiller131 <rkmiller131@gmail.com>
2024-09-02 11:30:30 -0700

    Adjusted commentary for art aficionado/social butterfly

--- src/events/achievementChecks/artAficionado.js
+++ src/events/achievementChecks/artAficionado.js
@@ -3,11 +3,12 @@ const isMessageArtRelated = require('../../utils/isMessageArtRelated');
 const { getUserDocument, updateUserChannels } = require('../../utils/server.collection');
 
 module.exports = async function checkArtAficionado(message, guildId, userId) {
-  const { user } = await getUserDocument(guildId, userId);
   const artRelatedChannel = isMessageArtRelated(message);
-  if (artRelatedChannel !== true) return;
+  if (artRelatedChannel !== true) return; // channel is art related AND has an attachement if true
 
+  const { user } = await getUserDocument(guildId, userId);
   const channelName = message.channel.name;
+
   if (user.channelsParticipatedIn.get(channelName) !== 10) return;
 
   await findAndGiveAchievement('Art Aficionado', user, message, guildId, userId);
--- src/events/achievementChecks/socialButterfly.js
+++ src/events/achievementChecks/socialButterfly.js
@@ -7,12 +7,11 @@ module.exports = async function checkSocialButterfly(message, guildId, userId) {
   // Because we manually created an art-text-channel, don't count this in addition to other art channels
   const channelKeys = Array.from(user.channelsParticipatedIn.keys());
   const userArtChannels = channelKeys.filter((key) => REGEX_ART.test(key) || ART_WORDS.includes(key));
-
+  // if the matching results (assuming we get back an array) includes 'art-text-only' then
+  // subtract one from the size during the check. otherwise normal check.
   const numChannels = (userArtChannels.length > 1 && userArtChannels.includes('art-text-only')) ?
     user.channelsParticipatedIn.size - 1 : user.channelsParticipatedIn.size;
 
-  // if the matching results (assuming we get back an array) includes 'art-text-only' then
-  // subtract one from the size during the check. otherwise normal check.
   if (numChannels !== 5) return;
 
   await findAndGiveAchievement('Social Butterfly', user, message, guildId, userId);
